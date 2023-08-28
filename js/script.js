30f9f3ecc7a2d989942a37bd9ecd7050f4eaa439
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-08-28 19:49:42 -0300

    updated finish :sparkles:

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let menu = document.querySelector("#menu-btn");
 let navbar = document.querySelector(".navbar");
+let header = document.querySelector(".header");
 
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
@@ -9,4 +10,8 @@ menu.onclick = () => {
 window.addEventListener("scroll",() => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
+});
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
