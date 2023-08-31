64a53e2819a6261419c29fdeca18126e0a492bae
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-08-31 09:47:38 -0300

    added array :sparkles:

--- js/script.js
+++ js/script.js
@@ -7,11 +7,40 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-window.addEventListener("scroll",() => {
+window.addEventListener("scroll", () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 });
 
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+const cards = [
+  { id: 1, name: "html", image: "icon-1.png" },
+  { id: 2, name: "css", image: "icon-2.png" },
+  { id: 3, name: "javascript", image: "icon-3.png" },
+  { id: 4, name: "react", image: "icon-6.png" },
+  { id: 5, name: "bootstrap", image: "icon-4.png" },
+  { id: 6, name: "github", image: "icon-5.png" },
+];
+
+// for (const card of cards) {
+//   const card = `<div class="post-box">
+//   <div class="img-box">
+//     <img src="image/${card.image}" alt="imagem html" class="post-image" />
+//   </div>
+//   <span>${card.name}</span>
+// </div>`;
+//   document.querySelector(".post-container").innerHTML += item;
+// }
+
+cards.map((card) => {
+  const getCards = `<div class="post-box">
+  <div class="img-box">
+     <img src="image/${card.image}" alt="imagem html" class="post-image" />
+   </div>
+   <span>${card.name}</span>
+ </div>`;
+ document.querySelector(".post-container").innerHTML += getCards
 });
